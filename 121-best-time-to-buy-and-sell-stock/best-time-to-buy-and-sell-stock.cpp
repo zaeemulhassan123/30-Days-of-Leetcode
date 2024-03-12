262a3c1381f4bd95fe1eb2f4a1cdf405efb3b45a
zaeemulhassan123 <66414430+zaeemulhassan123@users.noreply.github.com>
2024-03-12 22:33:20 +0500

    Time: 80 ms (83.79%) | Memory: 95.9 MB (28.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,18 @@
-#include <vector>
-#include <climits> // For INT_MAX
-
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
-        int minPrice = INT_MAX; // Initialize minPrice to INT_MAX
-        int maxProfit = 0; // Initialize maxProfit to 0
+    int maxProfit(vector<int>& prices) {
+        int minPrice = numeric_limits<int>::max();
+        int maxProfit = 0;
         
-        for (int price : prices) {
-            // Update minPrice to the minimum price seen so far
-            if (price < minPrice) {
+        for (int price : prices)
+        {
+            if (price < minPrice)
+            {
                 minPrice = price;
-            } else {
-                // Update maxProfit if selling at current price would yield higher profit
-                int currentProfit = price - minPrice;
-                if (currentProfit > maxProfit) {
-                    maxProfit = currentProfit;
-                }
+            }
+            else
+            {
+                 maxProfit = max(maxProfit, price - minPrice);
             }
         }
         
