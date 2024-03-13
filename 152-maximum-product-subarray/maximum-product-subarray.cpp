4f1ef6bee32bb8f81ab6e3d3222e2bc0406691f4
zaeemulhassan123 <66414430+zaeemulhassan123@users.noreply.github.com>
2024-03-14 02:53:26 +0500

    Time: 3 ms (86.34%) | Memory: 16.2 MB (41.97%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-               int n = nums.size();
+        int n = nums.size();
         if (n == 0) return 0;
         
-        int maxProductEndingHere = nums[0]; // Initialize max product ending at current index
-        int minProductEndingHere = nums[0]; // Initialize min product ending at current index
-        int maxProductSoFar = nums[0]; // Initialize max product seen so far
+        int maxProductEndingHere = nums[0];
+        int minProductEndingHere = nums[0];
+        int maxProductSoFar = nums[0];
         
-        for (int i = 1; i < n; ++i) {
-            // Update max and min products ending at current index
+        for (int i = 1; i < n; ++i)
+        {
             int tempMax = std::max(nums[i], std::max(maxProductEndingHere * nums[i], minProductEndingHere * nums[i]));
             minProductEndingHere = std::min(nums[i], std::min(maxProductEndingHere * nums[i], minProductEndingHere * nums[i]));
             
-            // Update max product seen so far
             maxProductSoFar = std::max(maxProductSoFar, tempMax);
             
-            // Update max product ending at current index for the next iteration
             maxProductEndingHere = tempMax;
         }
         
